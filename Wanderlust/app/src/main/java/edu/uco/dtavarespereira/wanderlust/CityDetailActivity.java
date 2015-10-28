5f6e6c99d6eda2373ac24d05f3b0e0033791203f
Danianny Gomes dos Santos <Dani@Daniannys-MacBook-Pro.local>
2015-10-27 19:55:12 -0500

    Added places to visit and search api

--- Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/CityDetailActivity.java
+++ Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/CityDetailActivity.java
@@ -2,9 +2,12 @@ package edu.uco.dtavarespereira.wanderlust;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -16,35 +19,54 @@ public class CityDetailActivity extends Activity {
     TextView tvCityName;
     Switch swFavorite;
     String city;
+    Button placesToVisit;
+    Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_detail);
 
+        location = new Location("");
+        location.setLatitude(51.503186);
+        location.setLongitude(-0.126446); //TODO ~ get the location lat and lng of the city and set in this variable
+
         tvCityName = (TextView) findViewById(R.id.tv_city_name);
         swFavorite = (Switch) findViewById(R.id.sw_favorite);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         city = intent.getStringExtra("CITY_NAME");
         tvCityName.setText(city);
 
         swFavorite.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     ArrayList<String> favorites = InitialActivity.getStringArrayPref(getApplicationContext(), "FAVORITES");
                     favorites.add(city);
-                    InitialActivity.setStringArrayPref(getApplicationContext(),"FAVORITES", favorites);
+                    InitialActivity.setStringArrayPref(getApplicationContext(), "FAVORITES", favorites);
 
                     Toast.makeText(getApplicationContext(), city + " was marked as favorite.", Toast.LENGTH_SHORT).show();
-                }else{
+                } else {
                     ArrayList<String> favorites = InitialActivity.getStringArrayPref(getApplicationContext(), "FAVORITES");
                     favorites.remove(city);
                     InitialActivity.setStringArrayPref(getApplicationContext(), "FAVORITES", favorites);
 
                     Toast.makeText(getApplicationContext(), city + " was removed from favorites.", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        placesToVisit = (Button) findViewById(R.id.btnPlacesToVisit);
+
+        placesToVisit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    Intent intent1 = new Intent(getApplication(), PlacesToVisit.class);
+                    intent1.putExtra("cityName", city);
+                    intent1.putExtra("locationLatitude", location.getLatitude());
+                    intent1.putExtra("locationLongitude", location.getLongitude());
+                    startActivity(intent1);
             }
         });
     }
