64a819c47305977741e892a391ef68e74c532b85
Matheus Campos Cardoso <matheuscamposcardoso@Matheuss-MacBook-Pro.local>
2015-10-27 23:07:07 -0500

    Adding Google Maps

--- Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/CommercialPlaces.java
+++ Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/CommercialPlaces.java
@@ -16,7 +16,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONException;
 
@@ -135,7 +134,13 @@ public class CommercialPlaces extends Activity {
         @Override
         protected void onPostExecute(ArrayList result) {
             //TODO
-            Toast.makeText(getApplicationContext(), location.toString(), Toast.LENGTH_SHORT).show();
+
+            Intent intentMaps = new Intent(CommercialPlaces.this, MapsActivity.class);
+            intentMaps.putExtra("lat", location.getLatitude());
+            intentMaps.putExtra("lon", location.getLongitude());
+
+            startActivity(intentMaps);
+
         }
 
     }
