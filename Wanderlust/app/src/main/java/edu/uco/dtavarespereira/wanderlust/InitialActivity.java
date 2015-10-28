a64d6b593fb65c2ff63454f6ca6ceaf4b0f10d68
danilofelicio <danillofelicio@hotmail.com>
2015-10-27 19:51:43 -0500

    JSON class implemented

--- Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/InitialActivity.java
+++ Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/InitialActivity.java
@@ -4,42 +4,51 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Outline;
-import android.os.Build;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewOutlineProvider;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class InitialActivity extends Activity {
-    ImageButton btAdd;
+
+    ImageButton btSearch;
     EditText etCityName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-        btAdd = (ImageButton) findViewById(R.id.bt_add);
-        etCityName = (EditText) findViewById(R.id.et_city);
+        btSearch = (ImageButton) findViewById(R.id.bt_search);
+        etCityName = (EditText)findViewById(R.id.editText_city);
 
-        //this is just a test
-        btAdd.setOnClickListener(new View.OnClickListener() {
+        btSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (etCityName.getText().toString().trim().equals("")){
+                if (etCityName.getText().toString().trim().equals("")) {
                     Toast.makeText(getApplicationContext(), "Please type a city", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
                     Intent intent = new Intent(InitialActivity.this, CityDetailActivity.class);
                     intent.putExtra("CITY_NAME", etCityName.getText().toString());
                     startActivity(intent);
@@ -103,4 +112,102 @@ public class InitialActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    class HttpGetTask extends AsyncTask<String, Void, ArrayList<String>> {
+
+        private static final String TAG = "HttpGetTask";
+        final String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?";
+
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+
+            InputStream in = null;
+            HttpURLConnection httpUrlConnection = null;
+            ArrayList<String> resultArray = null;
+
+            try {
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter("q",params[0] + ",us") // city
+                        .appendQueryParameter("mode", "json") // json format as result
+                        .appendQueryParameter("units", "metric") // metric unit
+                        .appendQueryParameter("APPID", "d5ec4c21045bdf4fbe86c6fd452fd299")
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+                httpUrlConnection = (HttpURLConnection) url.openConnection();
+                in = new BufferedInputStream(
+                        httpUrlConnection.getInputStream());
+                String data = readStream(in);
+                resultArray = JSonData.getData(data);
+
+            } catch (MalformedURLException exception) {
+                Log.e(TAG, "MalformedURLException");
+            } catch (IOException exception) {
+                Log.e(TAG, "IOException");
+            } catch (JSONException e) {
+                Log.e(TAG, e.getMessage(), e);
+                e.printStackTrace();
+            } finally {
+                if (null != httpUrlConnection) {
+                    httpUrlConnection.disconnect();
+                }
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return resultArray;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+            if (result == null || result.size() == 0) {
+                Toast.makeText(InitialActivity.this,
+                        "Invalid weather data. Possibly wrong city",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            //temp.setText("Temperature: " + result.get(2) + (char) 0x00B0);
+            //mapButton.setText("Lat: " + result.get(0) + " - " + "Long: " + result.get(1));
+            //desc.setText("Weather: " + result.get(4));
+            //speed.setText("Wind Speed: " + result.get(3));
+
+            //latitude = result.get(0);
+            //longitude = result.get(1);
+            //temperature = result.get(2);
+
+
+        }
+
+
+        private String readStream(InputStream in) {
+            BufferedReader reader = null;
+            StringBuffer data = new StringBuffer("");
+            try {
+                reader = new BufferedReader(new InputStreamReader(in));
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    data.append(line);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "IOException");
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return data.toString();
+        }
+    }
+
 }
