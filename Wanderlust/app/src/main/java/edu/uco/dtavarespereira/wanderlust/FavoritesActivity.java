ca3202aeb8ba319f0bc04ad7601dab2abadddb5e
diegotavarez <tavares.diego@outlook.com>
2015-11-03 19:39:23 -0600

    Search option in favoritesActivity actionBar

--- Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/FavoritesActivity.java
+++ Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/FavoritesActivity.java
@@ -1,11 +1,15 @@
 package edu.uco.dtavarespereira.wanderlust;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +34,16 @@ public class FavoritesActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_favorites, menu);
-        return true;
+        new MenuInflater(getApplication()).inflate(R.menu.menu_favorites, menu);
+
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -39,13 +51,16 @@ public class FavoritesActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                // search action
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
