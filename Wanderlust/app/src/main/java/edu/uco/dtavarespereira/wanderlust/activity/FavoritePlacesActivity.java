33e2cc405e31c8f3a0051cb378df4299919b40b4
Diego Tavares <diegotavarez@Diegos-MacBook-Pro.local>
2015-12-01 21:02:12 -0600

    idk

---
+++ Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/activity/FavoritePlacesActivity.java
@@ -0,0 +1,54 @@
+package edu.uco.dtavarespereira.wanderlust.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
+
+import edu.uco.dtavarespereira.wanderlust.R;
+import edu.uco.dtavarespereira.wanderlust.adapter.FavoriteCitiesAdapter;
+import edu.uco.dtavarespereira.wanderlust.adapter.FavoritePlacesAdapter;
+import edu.uco.dtavarespereira.wanderlust.entity.Place;
+
+public class FavoritePlacesActivity extends AppCompatActivity {
+    ListView listPlaces;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_places);
+
+        View rootView = findViewById(android.R.id.content);
+        List<Place> places = InitialActivity.getDBHelper().getPlaces();
+
+        listPlaces = (ListView) findViewById(R.id.list_cities);
+        FavoritePlacesAdapter adapter = new FavoritePlacesAdapter(getBaseContext(), places, rootView);
+        listPlaces.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_favorite_places, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Wanderlust/app/src/main/java/edu/uco/dtavarespereira/wanderlust/adapter/FavoritePlacesAdapter.java
@@ -0,0 +1,85 @@
+package edu.uco.dtavarespereira.wanderlust.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uco.dtavarespereira.wanderlust.JSonData;
+import edu.uco.dtavarespereira.wanderlust.R;
+import edu.uco.dtavarespereira.wanderlust.activity.CityDetailActivity;
+import edu.uco.dtavarespereira.wanderlust.entity.Place;
+
+public class FavoritePlacesAdapter extends BaseAdapter {
+    private List<Place> places;
+    private Context context;
+    private View parent;
+    public String cityName;
+
+    public FavoritePlacesAdapter(Context context, List<Place> places, View parent){
+        this.context = context;
+        this.places = places;
+        this.parent = parent;
+    }
+
+    @Override
+    public int getCount(){
+        return places.size();
+    }
+
+    public Object getItem(int position){
+        return places.get(position);
+    }
+
+    public long getItemId(int position){
+        return position;
+    }
+
+    public View getView(int position, View converView, final ViewGroup parent)
+    {
+        View view = converView;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.favorites_cities_item, parent, false);
+
+        final TextView tvCityName = (TextView) view.findViewById(R.id.tv_city_name_item);
+
+        Place place = places.get(position);
+
+        tvCityName.setText(capitalize(place.getName()));
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+
+    }
+
+
+    private String capitalize(final String line) {
+        return Character.toUpperCase(line.charAt(0)) + line.substring(1);
+    }
+}
